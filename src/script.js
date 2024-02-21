09a36bc72ed9ec6deb9800a59e15f25bbee41ef5
Husam Solachuddin <solachuh@uci.edu>
2024-02-20 21:54:51 -0800

    visual changes

--- src/script.js
+++ src/script.js
@@ -6,20 +6,53 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-function flyPlane() {
-    const plane = document.querySelector('.plane');
-    plane.classList.add('flying');
+// function flyPlane() {
+//     const plane = document.querySelector('.plane');
+//     plane.classList.add('flying');
 
-    plane.addEventListener('animationend', function() {
-        plane.classList.remove('flying');
-    });
-}
+//     plane.addEventListener('animationend', function() {
+//         plane.classList.remove('flying');
+//     });
+// }
 
 function sendEmail() {
     window.location.href = "mailto:husam.solachuddin@gmail.com";
 }
 
+window.addEventListener('scroll', function() {
+    var logoContainer = document.querySelector('.logo-container');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        logoContainer.classList.add('fixed');
+    } else {
+        logoContainer.classList.remove('fixed');
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     const logo = document.getElementById('logo');
     logo.classList.add('loaded');
 });
+
+const pfp = document.querySelector('.main-pfp');
+
+function getRandomBorderRadius() {
+    const tlh = 40 + Math.random() * 55;
+    const trh = 40 + Math.random() * 55;
+    const brh = 35 + Math.random() * 55;
+    const blh = 45 + Math.random() * 55;
+    const tlv = 40 + Math.random() * 55;
+    const trv = 40 + Math.random() * 55;
+    const brv = 40 + Math.random() * 55;
+    const blv = 40 + Math.random() * 55;
+    return `${tlh}% ${trh}% ${brh}% ${blh}% / ${tlv}% ${trv}% ${brv}% ${blv}%`;
+}
+
+
+function applyRandomBorderRadius() {
+    pfp.style.borderRadius = getRandomBorderRadius();
+}
+
+applyRandomBorderRadius();
+setInterval(applyRandomBorderRadius, 2000);
